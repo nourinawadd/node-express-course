4af60ce1009048574e16ab524db9bcbc8fbac6f1
nourinawadd <nourinawad@gmail.com>
2025-04-26 21:07:49 +0300

    http request object

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,10 @@
-console.log('Express Tutorial')
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res)=>{
+    res.send('they hit the pentagon')
+})
+
+app.listen(5000, ()=>{
+    console.log('Server is listening...')
+})
